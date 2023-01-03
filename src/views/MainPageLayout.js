8879d285f2a26d4b8ed6ff486c6434d49432fb90
Dmitry Zirjanov <1296mhz@gmail.com>
2023-01-03 09:46:59 +0500

    Processing

--- src/views/MainPageLayout.js
+++ src/views/MainPageLayout.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Root from '../routes/root';
 import ErrorPage from './error-page';
 import SignUp from './SignUp';
-import SignInSide from './SignInSide';
+import Login from './Login';
 import Profile from './Profile';
 import {
   createBrowserRouter,
   RouterProvider,
+  Routes, Route, Navigate, useNavigate, useLocation
 } from "react-router-dom";
-
+import { history } from '../_helpers';
 import {createTheme, styled, ThemeProvider} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MuiAppBar from '@mui/material/AppBar';
@@ -18,6 +19,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -30,7 +32,7 @@ const router = createBrowserRouter([
   },
   {
     path: "signin",
-    element: <SignInSide />,
+    element: <Login />,
   },
   {
     path: "profile",
@@ -57,6 +59,9 @@ const AppBar = styled(MuiAppBar, {
 
 
 function MainPageLayout() {
+
+  history.navigate = useNavigate();
+  history.location = useLocation();
   //const theme = useTheme();
   const theme = createTheme({
     status: {
@@ -90,9 +95,9 @@ function MainPageLayout() {
         <CssBaseline />
         <AppBar position="fixed">
           <Toolbar>
-          <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
-            Welcome - Admin Panel One!
-          </Typography>
+            <Typography variant="h6" noWrap sx={{flexGrow: 1}} component="div">
+              Welcome - Admin Panel One!
+            </Typography>
             <Stack spacing={1} direction="row" justifyContent="flex-end" alignItems="center">
               <Button color="neutral" variant="outlined" href="/signin">Sign-In</Button>
               <Button color="neutral" variant="outlined" href="/signup">Sign-Up</Button>
